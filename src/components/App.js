6e5525b57b055b6cc24695812aa2256d68d3b49f
AdrianCon <adrian.dev@tutanota.com>
2024-03-22 12:32:00 -0600

    Add redux folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,7 @@
-import * as React from 'react'
+import * as React from "react";
+import { useSelector } from "react-redux";
 
 export default function App() {
-  return (
-    <div>
-      Redux Polls
-    </div>
-  )
+  const store = useSelector((state) => state);
+  return <div>Redux Polls</div>;
 }
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import authedUser from "./autherUser";
+
+export default combineReducers({
+  authedUser,
+});
